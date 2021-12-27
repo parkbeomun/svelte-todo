437d167cfedf20d3f2908caec346fb80cae9231d
unknown <sos891214@cnspartner.com>
2021-12-27 16:12:54 +0900

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,78 @@
-<script>
-	export let name;
+<script lang="ts">
+	import { Icon, Styles, Form, FormGroup, FormText, Input, Label } from 'sveltestrap';
+	const ENTER_KEY = 13;
+	const ESCAPE_KEY = 27;
+
+	let currentFilter = 'all'
+	let items = [];
+	let editing = null;
+
+
+	function createNew(event) {
+		if(event.which == ENTER_KEY) {
+			items = items.concat({
+				id: uuid(),
+				description: event.target.value,
+				completed: false
+			})
+
+			event.target.value = '';
+			console.log("items > "+items)
+		}
+	}
+
+	function handleEdit() {
+
+	}
+
+	function submit() {
+
+	}
+
+	function remove(index) {
+		items = items.slice(0, index).concat(items.slice(index + 1));
+	}
+
+	function uuid() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
+	}
+
+	$: filtered = currentFilter === 'all'
+		? items
+		: currentFilter === 'completed'
+			? items.filter(item => item.completed)
+			: items.filter(item => !item.completed)
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Todos</h1>
+		<Input
+			on:keydown={createNew}/>
+		{#if items.length > 0}
+			<ul class="todo-list">
+				{#each filtered as item, index (item.id)}
+					<li class="{item.completed ? 'completed' : ''} {editing === index ? 'editing' : ''}">
+						<div class="view">
+							<input class="toggle" type="checkbox" bind:checked={item.completed}>
+							<label on:dblclick="{() => editing = index}">{item.description}</label>
+							<button on:click="{() => remove(index)}">X</button>
+						</div>
+
+						{#if editing === index}
+							<input
+									value='{item.description}'
+									id="edit"
+									class="edit"
+									on:keydown={handleEdit}
+									on:blur={submit}
+									autofocus
+							>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		{/if}
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
